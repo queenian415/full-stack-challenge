84566f60dc9ccedac8d473d73df7c2b3b44ac2ba
Chu Xi Huang <queenian415@gmail.com>
2017-10-28 23:41:46 -0400

    removing unused files

--- database/insert_admin_employee.sql
+++
@@ -1,8 +0,0 @@
-USE full_stack;
-
-INSERT INTO Admin (name) VALUES ('Gin');
-
-INSERT INTO Employee (name, adminId)
-VALUES
-('Mary Land', 1), ('Harry Rose', 1), ('Sory So', 1), ('Hippie Hoppie', 1), ('Artie T', 1);
-
